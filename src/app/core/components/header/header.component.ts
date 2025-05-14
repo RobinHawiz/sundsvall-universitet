51575665d1aa8b6535d3a8aa4292557123978c7a
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2025-05-14 19:07:46 +0200

    ⚡️ perf(header): load logo dynamically based on theme
    Previously, both light and dark logos loaded regardless of mode.
    Now uses [src] binding to load only the appropriate logo, reducing overhead.

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { material } from '@core/material';
+
+type Mode = 'dark' | 'light';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +10,31 @@ import { material } from '@core/material';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  logoSrc = signal('');
+
+  ngOnInit() {
+    this.setInitLogoMode();
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', (event) => {
+        const mode = event.matches ? 'dark' : 'light';
+        this.toggleLogoMode(mode);
+      });
+  }
+
+  setInitLogoMode() {
+    const mode = window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light';
+    this.toggleLogoMode(mode);
+  }
+
+  toggleLogoMode(mode: Mode): void {
+    this.logoSrc.set(
+      mode === 'dark'
+        ? 'sundsvall_universitet_logga_light.svg'
+        : 'sundsvall_universitet_logga_dark.svg'
+    );
+  }
+}
