3cd85cb112f30aacbada16b403f7e3d35a97c0a3
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2025-05-14 14:31:29 +0200

    🚸 feat(kurser): improve UX with feedback messages and loading spinner
    Adds loading indicator and clearer messages for better user interaction flow.

--- src/app/core/material.ts
+++ src/app/core/material.ts
@@ -4,6 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 export const material = [
   MatTableModule,
@@ -12,4 +13,5 @@ export const material = [
   MatInputModule,
   MatFormFieldModule,
   MatSelectModule,
+  MatProgressSpinnerModule,
 ];
